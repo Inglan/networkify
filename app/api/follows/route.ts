69f41adf32fbf5a925d99ec86801066bd577982c
Ingo Wolf <me@ingowolf.au>
2025-10-01 23:35:00 +1000

    add endpoint for follows

---
+++ app/api/follows/route.ts
@@ -0,0 +1,28 @@
+import {
+  getCurrentUsername,
+  getUserFollowing,
+  getUserFollowers,
+} from "@/lib/utils";
+
+export async function POST(req: Request) {
+  const reqBody = await req.text();
+  try {
+    JSON.parse(reqBody);
+  } catch (error) {
+    return Response.json(
+      { error: true, response: "Invalid JSON" },
+      { status: 400 },
+    );
+  }
+  const { token, username } = await JSON.parse(reqBody);
+  if (!(token && username)) {
+    return Response.json(
+      { error: true, response: "Token or username not provided" },
+      { status: 400 },
+    );
+  }
+  return Response.json({
+    following: await getUserFollowing(token, username),
+    followers: await getUserFollowers(token, username),
+  });
+}
