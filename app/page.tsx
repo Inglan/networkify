d81ef835459228a4b07a0abeb1c8686448f6c519
Ingo Wolf <me@ingowolf.au>
2025-10-02 00:09:00 +1000

    start working on basic functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GraphCanvas } from "reagraph";
+import { getUser } from "./actions";
 
 export default function Home() {
   const [nodes, setNodes] = useState<{ id: string; label: string }[]>([]);
-
   const [edges, setEdges] = useState<
     { source: string; target: string; id: string; label: string }[]
   >([]);
 
+  const [token, setToken] = useState<string>("");
+
+  useEffect(() => {
+    console.log(nodes);
+  }, [nodes]);
+
   return (
     <>
-      <GraphCanvas nodes={nodes} edges={edges} />
+      Token
+      <input
+        className="p-2 border"
+        type="text"
+        value={token}
+        onChange={(e) => setToken(e.target.value)}
+      />
+      <br />
+      <button
+        onClick={() =>
+          getUser(token).then((data) => {
+            setNodes([
+              ...nodes,
+              {
+                id: data.username,
+                label: data.name,
+              },
+            ]);
+            console.log(nodes);
+          })
+        }
+      >
+        test
+      </button>
+      <br />
+      {JSON.stringify(nodes)}
+      <div style={{ position: "fixed", width: "75%", height: "75%" }}>
+        <GraphCanvas nodes={nodes} edges={edges} />
+      </div>
     </>
   );
 }
