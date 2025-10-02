dd1bd3c9a9771643486c10f58f7ce8805a94c8c7
Ingo Wolf <me@ingowolf.au>
2025-10-02 11:10:34 +1000

    add statistics

--- app/page.tsx
+++ app/page.tsx
@@ -179,6 +179,8 @@ export default function Home() {
         >
           Load
         </Button>
+        <div>{nodes.length} nodes</div>
+        <div>{edges.length} edges</div>
       </div>
       <GraphCanvas
         labelType="nodes"
