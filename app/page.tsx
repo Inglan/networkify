520ffceca8af264948ea66669fa8304d28cdf96d
Ingo Wolf <me@ingowolf.au>
2025-10-07 18:36:50 +1100

    remove debuggign useres thing

--- app/page.tsx
+++ app/page.tsx
@@ -583,7 +583,6 @@ export default function Home() {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
-        {JSON.stringify(users)}
       </div>
       <Button
         size="icon"
