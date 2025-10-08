39f79f26031438a59310ad07175fea27feda6994
Ingo Wolf <me@ingowolf.au>
2025-10-08 17:38:38 +1100

    move updategraph to anotehr file

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,7 @@ import { Kbd } from "@/components/ui/kbd";
 import { toast } from "sonner";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Edges, Nodes, Users } from "@/lib/types";
+import { updateGraph as updateGraphFunction } from "@/lib/graphUtils";
 
 export default function Home() {
   const graphRef = useRef<GraphCanvasRef | null>(null);
@@ -75,6 +76,9 @@ export default function Home() {
       }
     });
   };
+
+  const updateGraph = () =>
+    updateGraphFunction(nodes, setNodes, edges, setEdges, users);
 
   useHotkeys("mod+f", () => openAccordion("search"), {
     preventDefault: true,
@@ -137,91 +141,6 @@ export default function Home() {
     } finally {
       setActiveOperations((prev) => prev - 1);
     }
-  }
-
-  function updateGraph(
-    nodes: Nodes,
-    setNodes: Dispatch<SetStateAction<Nodes>>,
-    edges: Edges,
-    setEdges: Dispatch<SetStateAction<Edges>>,
-    users: Users,
-  ) {
-    let updatedNodes: typeof nodes = [];
-    let updatedEdges: typeof edges = [];
-    users.forEach((user) => {
-      let fillColor: string;
-      switch (user.searchState) {
-        case "not_searched":
-          fillColor = "gray";
-          break;
-        case "searching":
-          fillColor = "blue";
-          break;
-        case "searched":
-          fillColor = "green";
-          break;
-        case "error":
-          fillColor = "red";
-          break;
-        default:
-          fillColor = "gray";
-      }
-      updatedNodes.push({
-        id: user.username,
-        label: user.name,
-        fill: fillColor,
-      });
-
-      if (user.followers.length < 100) {
-        user.followers.forEach((follower) => {
-          if (!follower.username.startsWith("spotify:artist")) {
-            const followerId = follower.username.replace("spotify:user:", "");
-            const userId = user.username.replace("spotify:user:", "");
-            const edgeId = `${followerId}-${userId}`;
-            if (!updatedEdges.some((edge) => edge.id === edgeId)) {
-              updatedEdges.push({
-                id: edgeId,
-                source: followerId,
-                target: userId,
-                label: "Following",
-              });
-            }
-          }
-        });
-      }
-
-      if (user.following.length < 100) {
-        user.following.forEach((following) => {
-          if (!following.username.startsWith("spotify:artist")) {
-            const userId = user.username.replace("spotify:user:", "");
-            const followingId = following.username.replace("spotify:user:", "");
-            const edgeId = `${userId}-${followingId}`;
-            if (!updatedEdges.some((edge) => edge.id === edgeId)) {
-              updatedEdges.push({
-                id: edgeId,
-                source: userId,
-                target: followingId,
-                label: "Following",
-              });
-            }
-          }
-        });
-      }
-    });
-
-    updatedNodes.forEach((node) => {
-      if (
-        !updatedEdges.some(
-          (edge) => edge.source === node.id || edge.target === node.id,
-        )
-      ) {
-        updatedNodes = updatedNodes.filter((n) => n.id !== node.id);
-      }
-    });
-
-    setNodes(updatedNodes);
-    setEdges(updatedEdges);
-    toast.success("Graph updated");
   }
 
   useEffect(() => {
---
+++ lib/graphUtils.ts
@@ -0,0 +1,87 @@
+import { Dispatch, SetStateAction } from "react";
+import { Edges, Nodes, Users } from "./types";
+
+export function updateGraph(
+  nodes: Nodes,
+  setNodes: Dispatch<SetStateAction<Nodes>>,
+  edges: Edges,
+  setEdges: Dispatch<SetStateAction<Edges>>,
+  users: Users,
+) {
+  let updatedNodes: typeof nodes = [];
+  let updatedEdges: typeof edges = [];
+  users.forEach((user) => {
+    let fillColor: string;
+    switch (user.searchState) {
+      case "not_searched":
+        fillColor = "gray";
+        break;
+      case "searching":
+        fillColor = "blue";
+        break;
+      case "searched":
+        fillColor = "green";
+        break;
+      case "error":
+        fillColor = "red";
+        break;
+      default:
+        fillColor = "gray";
+    }
+    updatedNodes.push({
+      id: user.username,
+      label: user.name,
+      fill: fillColor,
+    });
+
+    if (user.followers.length < 100) {
+      user.followers.forEach((follower) => {
+        if (!follower.username.startsWith("spotify:artist")) {
+          const followerId = follower.username.replace("spotify:user:", "");
+          const userId = user.username.replace("spotify:user:", "");
+          const edgeId = `${followerId}-${userId}`;
+          if (!updatedEdges.some((edge) => edge.id === edgeId)) {
+            updatedEdges.push({
+              id: edgeId,
+              source: followerId,
+              target: userId,
+              label: "Following",
+            });
+          }
+        }
+      });
+    }
+
+    if (user.following.length < 100) {
+      user.following.forEach((following) => {
+        if (!following.username.startsWith("spotify:artist")) {
+          const userId = user.username.replace("spotify:user:", "");
+          const followingId = following.username.replace("spotify:user:", "");
+          const edgeId = `${userId}-${followingId}`;
+          if (!updatedEdges.some((edge) => edge.id === edgeId)) {
+            updatedEdges.push({
+              id: edgeId,
+              source: userId,
+              target: followingId,
+              label: "Following",
+            });
+          }
+        }
+      });
+    }
+  });
+
+  updatedNodes.forEach((node) => {
+    if (
+      !updatedEdges.some(
+        (edge) => edge.source === node.id || edge.target === node.id,
+      )
+    ) {
+      updatedNodes = updatedNodes.filter((n) => n.id !== node.id);
+    }
+  });
+
+  setNodes(updatedNodes);
+  setEdges(updatedEdges);
+  toast.success("Graph updated");
+}
