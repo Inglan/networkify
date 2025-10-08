71253566f21674c4cda44d6ab3fdad47ad708050
Ingo Wolf <me@ingowolf.au>
2025-10-09 10:42:02 +1100

    move discover function

--- app/page.tsx
+++ app/page.tsx
@@ -97,55 +97,14 @@ export default function Home() {
     preventDefault: true,
   });
 
-  async function discover(username: string) {
-    if (!token) return;
-    if (username.startsWith("spotify:artist")) return;
-    setActiveOperations((prev) => prev + 1);
-    updateUserState(username, { searchState: "searching" });
-    try {
-      const { followers, following } = await getFollows(token, username);
-      // Todo: Limit following (users only, not artists)
-      updateUserState(username, {
-        searchState: "searched",
-        followers,
-        following,
-      });
-      [...followers, ...following]
-        .filter((user) => !user.username.startsWith("spotify:artist"))
-        .forEach((user) => {
-          if (
-            !createUser({
-              ...user,
-              username: user.username.replace("spotify:user:", ""),
-              followers: [],
-              following: [],
-              searchState: "not_searched",
-              exclude_from_graph:
-                followers.length > 100 ||
-                following.filter(
-                  (user) => !user.username.startsWith("spotify:artist"),
-                ).length > 100,
-            }) &&
-            !(
-              followers.length > 100 ||
-              following.filter(
-                (user) => !user.username.startsWith("spotify:artist"),
-              ).length > 100
-            )
-          ) {
-            updateUserState(user.username, {
-              exclude_from_graph: false,
-            });
-          }
-        });
-    } catch (error) {
-      updateUserState(username, { searchState: "error" });
-      toast.error("Something went wrong. Maybe aquire another token.");
-      console.error("Error occurred while fetching data", error);
-    } finally {
-      setActiveOperations((prev) => prev - 1);
-    }
-  }
+  const discover = (username: string) =>
+    spotify.discover(
+      username,
+      token,
+      setActiveOperations,
+      updateUserState,
+      createUser,
+    );
 
   useEffect(() => {
     if (activeOperations === 0) {
