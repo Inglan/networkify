c2c6c91cc7fd1ddbc7f03e04dba5d89f9382679a
Ingo Wolf <me@ingowolf.au>
2025-10-09 09:36:55 +1100

    add userutils

---
+++ lib/userUtils.ts
@@ -0,0 +1,35 @@
+import { Dispatch, SetStateAction } from "react";
+import { Users } from "./types";
+
+export function updateUserState(
+  username: string,
+  newState: Partial<Users[number]>,
+  setUsers: Dispatch<SetStateAction<Users>>,
+) {
+  setUsers((prev) => {
+    const userIndex = prev.findIndex(
+      (user) => user.username === username.replace("spotify:user:", ""),
+    );
+    if (userIndex === -1) return prev;
+    return [
+      ...prev.slice(0, userIndex),
+      { ...prev[userIndex], ...newState },
+      ...prev.slice(userIndex + 1),
+    ];
+  });
+}
+
+export function createUser(
+  userData: Users[number],
+  setUsers: Dispatch<SetStateAction<Users>>,
+) {
+  let created = false;
+  setUsers((prev) => {
+    if (!prev.some((user) => user.username === userData.username)) {
+      created = true;
+      return [...prev, userData];
+    }
+    return prev;
+  });
+  return created;
+}
