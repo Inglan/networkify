4940c221e4dc58a5b9a8fa9f9438e3d87cbae8f3
Ingo Wolf <me@ingowolf.au>
2025-10-09 10:55:03 +1100

    move data to component

--- app/page.tsx
+++ app/page.tsx
@@ -145,63 +145,12 @@ export default function Home() {
           <AccordionItem value="Data">
             <AccordionTrigger className="px-4">Data</AccordionTrigger>
             <AccordionContent className="p-4">
-              <div className=" flex flex-col gap-2">
-                <Button
-                  onClick={() => {
-                    setUsers([]);
-                  }}
-                >
-                  Clear all
-                </Button>
-                <Button
-                  onClick={() => {
-                    const data = {
-                      users,
-                    };
-
-                    const blob = new Blob([JSON.stringify(data)], {
-                      type: "text/plain;charset=utf-8",
-                    });
-
-                    const url = URL.createObjectURL(blob);
-                    const link = document.createElement("a");
-                    link.href = url;
-                    link.download = `networkify-${new Date().toISOString()}.json`;
-                    link.click();
-                    URL.revokeObjectURL(url);
-                  }}
-                >
-                  Save
-                </Button>
-                <Button
-                  onClick={() => {
-                    const input = document.createElement("input");
-                    input.type = "file";
-                    input.accept = ".json";
-                    input.onchange = async (event) => {
-                      const file = (event.target as HTMLInputElement)
-                        .files?.[0];
-                      if (!file) return;
-
-                      const reader = new FileReader();
-                      reader.onload = async (event) => {
-                        const data = JSON.parse(event.target?.result as string);
-                        setUsers(data.users);
-                      };
-                      reader.readAsText(file);
-                    };
-                    input.click();
-                  }}
-                >
-                  Load
-                </Button>
-                <div>
-                  You need to manually update the graph after loading data.
-                </div>
-                <div>{nodes.length} nodes</div>
-                <div>{edges.length} edges</div>
-                <div>{users.length} users</div>
-              </div>
+              <Data
+                edges={edges}
+                nodes={nodes}
+                setUsers={setUsers}
+                users={users}
+              />
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="search">
@@ -673,6 +622,75 @@ function Discover({
         )
       </Button>
       <div>{activeOperations} active searches</div>
+    </div>
+  );
+}
+
+function Data({
+  setUsers,
+  users,
+  nodes,
+  edges,
+}: {
+  setUsers: (users: Users) => void;
+  users: Users;
+  nodes: Nodes;
+  edges: Edges;
+}) {
+  return (
+    <div className=" flex flex-col gap-2">
+      <Button
+        onClick={() => {
+          setUsers([]);
+        }}
+      >
+        Clear all
+      </Button>
+      <Button
+        onClick={() => {
+          const data = {
+            users,
+          };
+
+          const blob = new Blob([JSON.stringify(data)], {
+            type: "text/plain;charset=utf-8",
+          });
+
+          const url = URL.createObjectURL(blob);
+          const link = document.createElement("a");
+          link.href = url;
+          link.download = `networkify-${new Date().toISOString()}.json`;
+          link.click();
+          URL.revokeObjectURL(url);
+        }}
+      >
+        Save
+      </Button>
+      <Button
+        onClick={() => {
+          const input = document.createElement("input");
+          input.type = "file";
+          input.accept = ".json";
+          input.onchange = async (event) => {
+            const file = (event.target as HTMLInputElement).files?.[0];
+            if (!file) return;
+
+            const reader = new FileReader();
+            reader.onload = async (event) => {
+              const data = JSON.parse(event.target?.result as string);
+              setUsers(data.users);
+            };
+            reader.readAsText(file);
+          };
+          input.click();
+        }}
+      >
+        Load
+      </Button>
+      <div>You need to manually update the graph after loading data.</div>
+      <div>{nodes.length} nodes</div>
+      <div>{edges.length} edges</div>
+      <div>{users.length} users</div>
     </div>
   );
 }
