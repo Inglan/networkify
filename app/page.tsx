9f98a17ebd9d925b848e1fcd5b51f80a01c120c6
Ingo Wolf <me@ingowolf.au>
2025-10-11 18:01:29 +1100

    add another div to fix alignment issues

--- app/page.tsx
+++ app/page.tsx
@@ -231,87 +231,89 @@ function GettingStartedDialog({
         <DialogHeader>
           <DialogTitle className="sr-only">Getting started</DialogTitle>
         </DialogHeader>
-        <div className="overflow-auto h-96 flex items-center">
-          {page == 0 && (
-            <div className="p-14 w-full flex flex-col items-center text-center gap-4">
-              <div className="text-3xl">Welcome to networkify</div>
-              <div className="text-md">
-                networkify is a tool for visualizing your Spotify followers and
-                follows
+        <div className="h-96 flex items-center">
+          <div className="overflow-auto max-h-96">
+            {page == 0 && (
+              <div className="p-14 w-full flex flex-col items-center text-center gap-4">
+                <div className="text-3xl">Welcome to networkify</div>
+                <div className="text-md">
+                  networkify is a tool for visualizing your Spotify followers
+                  and follows
+                </div>
+                <Button onClick={() => setPage(1)}>Get started</Button>
               </div>
-              <Button onClick={() => setPage(1)}>Get started</Button>
-            </div>
-          )}
-          {page == 1 && (
-            <div className="p-5 w-full flex flex-col items-center text-center gap-4">
-              <div className="text-xl">
-                To use networkify, you need a Spotify token
+            )}
+            {page == 1 && (
+              <div className="p-5 w-full flex flex-col items-center text-center gap-4">
+                <div className="text-xl">
+                  To use networkify, you need a Spotify token
+                </div>
+                <div className="text-sm">
+                  This is due to Spotify API limitations. It expires after a
+                  short amount of time and we don't store it.
+                </div>
+                <div className="flex flex-col md:flex-row gap-4">
+                  <Button disabled variant="outline">
+                    Use example data (coming soon)
+                  </Button>
+                  <Button onClick={() => setPage(2)}>Show me how</Button>
+                </div>
               </div>
-              <div className="text-sm">
-                This is due to Spotify API limitations. It expires after a short
-                amount of time and we don't store it.
+            )}
+            {page == 2 && (
+              <div className="p-5 w-full flex flex-col gap-4 text-left">
+                <div className="text-xl">How to get a Spotify token</div>
+                <div>
+                  <ol className="list-decimal list-inside">
+                    <li>
+                      Open Spotify web in a Firefox based browser and log in
+                    </li>
+                    <li>
+                      Open developer tools with <Kbd>Ctrl + Shift + I</Kbd>
+                    </li>
+                    <li>Go to the network tab</li>
+                    <li>Refresh spotify, then wait a few seconds</li>
+                    <li>Right click on one of the entries</li>
+                    <li>Select Copy Value then Copy All As HAR</li>
+                    <li>
+                      Paste in here (or in the token field up the top right
+                      later on):
+                    </li>
+                  </ol>
+                </div>
+                <Textarea
+                  onPaste={(e) => {
+                    e.preventDefault();
+                    try {
+                      setToken(tokenUtils.getFromClipboard(e));
+                      setPage(3);
+                    } catch {
+                      toast.error("Invalid data pasted, please try again");
+                    }
+                  }}
+                  placeholder="Paste here"
+                />
+                <div className="text-sm">
+                  Disclaimer: It is generally not recommended to do random stuff
+                  in devtools because a random website asked you to. This could
+                  allow me to get access to your spotify account if I were
+                  collecting tokens on the server. If you don't trust me (theres
+                  no reason you should), then you can audit the code, which is{" "}
+                  <Link
+                    href="https://github.com/Inglan/networkify"
+                    target="_blank"
+                    className="underline"
+                    rel="noopener noreferrer"
+                  >
+                    open source
+                  </Link>{" "}
+                  (GPL-v3), and run it locally, or simply not use this tool.
+                  This tool may also be against the terms of service of Spotify,
+                  so be careful :)
+                </div>
               </div>
-              <div className="flex flex-col md:flex-row gap-4">
-                <Button disabled variant="outline">
-                  Use example data (coming soon)
-                </Button>
-                <Button onClick={() => setPage(2)}>Show me how</Button>
-              </div>
-            </div>
-          )}
-          {page == 2 && (
-            <div className="p-5 w-full flex flex-col gap-4 text-left">
-              <div className="text-xl">How to get a Spotify token</div>
-              <div>
-                <ol className="list-decimal list-inside">
-                  <li>
-                    Open Spotify web in a Firefox based browser and log in
-                  </li>
-                  <li>
-                    Open developer tools with <Kbd>Ctrl + Shift + I</Kbd>
-                  </li>
-                  <li>Go to the network tab</li>
-                  <li>Refresh spotify, then wait a few seconds</li>
-                  <li>Right click on one of the entries</li>
-                  <li>Select Copy Value then Copy All As HAR</li>
-                  <li>
-                    Paste in here (or in the token field up the top right later
-                    on):
-                  </li>
-                </ol>
-              </div>
-              <Textarea
-                onPaste={(e) => {
-                  e.preventDefault();
-                  try {
-                    setToken(tokenUtils.getFromClipboard(e));
-                    setPage(3);
-                  } catch {
-                    toast.error("Invalid data pasted, please try again");
-                  }
-                }}
-                placeholder="Paste here"
-              />
-              <div className="text-sm">
-                Disclaimer: It is generally not recommended to do random stuff
-                in devtools because a random website asked you to. This could
-                allow me to get access to your spotify account if I were
-                collecting tokens on the server. If you don't trust me (theres
-                no reason you should), then you can audit the code, which is{" "}
-                <Link
-                  href="https://github.com/Inglan/networkify"
-                  target="_blank"
-                  className="underline"
-                  rel="noopener noreferrer"
-                >
-                  open source
-                </Link>{" "}
-                (GPL-v3), and run it locally, or simply not use this tool. This
-                tool may also be against the terms of service of Spotify, so be
-                careful :)
-              </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </DialogContent>
     </Dialog>
