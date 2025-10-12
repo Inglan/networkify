63c6062e6768c5c782023335d615005a3fb335df
Ingo Wolf <me@ingowolf.au>
2025-10-12 14:05:17 +1100

    add saving functionality

--- components/sidebar/Data.tsx
+++ components/sidebar/Data.tsx
@@ -3,6 +3,13 @@
 import { Button } from "@/components/ui/button";
 import * as dataUtils from "@/lib/dataUtils";
 import { Edges, Nodes, Save, Users } from "@/lib/types";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Input } from "../ui/input";
 
 export function Data({
   setUsersAction: setUsers,
@@ -52,6 +59,38 @@ export function Data({
       <div>{nodes.length} nodes</div>
       <div>{edges.length} edges</div>
       <div>{users.length} users</div>
+
+      <form
+        className="flex flex-row gap-2 required"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formElement = e.target as HTMLFormElement;
+          const formData = new FormData(formElement);
+          const data = Object.fromEntries(formData.entries());
+          const saveName = data.name.toString();
+          if (!saveName) return;
+          setSave((current) => {
+            const newSave = current;
+            newSave.push({ name: saveName, date: Date.now(), data: { users } });
+            return newSave;
+          });
+          formElement.reset();
+        }}
+      >
+        <Input required name="name" placeholder="Save name" />{" "}
+        <Button>Save</Button>
+      </form>
+      <Accordion type="single" collapsible className="border rounded-md px-4">
+        <AccordionItem value="saves">
+          <AccordionTrigger>Saves</AccordionTrigger>
+          <AccordionContent>
+            {save.map((s) => (
+              <div key={s.date}>{s.name}</div>
+            ))}
+            {save.length === 0 && <div>No saves available</div>}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
