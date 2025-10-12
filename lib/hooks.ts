4a62aead5d0cedbf45d6e832fb263fde70b066f2
Ingo Wolf <me@ingowolf.au>
2025-10-12 13:26:15 +1100

    create presisted state hook

---
+++ lib/hooks.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function setItem(key: string, value: unknown) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error("Error saving to localStorage", error);
+  }
+}
+
+function getItem(key: string) {
+  try {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : undefined;
+  } catch (error) {
+    console.error("Error reading from localStorage", error);
+  }
+}
+
+function usePersistedState<T>(key: string, initialValue: T) {
+  const [state, setState] = useState<T>(() => {
+    return getItem(key) ?? initialValue;
+  });
+
+  useEffect(() => {
+    setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState] as const;
+}
+
+export default usePersistedState;
