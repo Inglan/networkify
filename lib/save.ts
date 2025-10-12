1a26bf0695fa1b7e0bac5d98d2877325a1b55620
Ingo Wolf <me@ingowolf.au>
2025-10-12 14:40:50 +1100

    add save state

---
+++ lib/save.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+import { User } from "./types";
+
+interface SaveState {
+  save: {
+    data: { users: User[] };
+    name: string;
+    timestamp: number;
+    id: string;
+  }[];
+  create: (data: { users: User[] }, name: string) => void;
+  delete: (id: string) => void;
+}
+
+const save = create<SaveState>((set) => ({
+  save: [],
+  create: (data, name) => {
+    const id = crypto.randomUUID();
+    set((state) => {
+      return {
+        save: [...state.save, { data, name, timestamp: Date.now(), id }],
+      };
+    });
+  },
+  delete: (id) => {
+    set((state) => {
+      return {
+        save: state.save.filter((item) => item.id !== id),
+      };
+    });
+  },
+}));
